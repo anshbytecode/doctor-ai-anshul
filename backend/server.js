7d59d6041c07d90bc3794a5cf439e09199921a2f
Anshul Bhilare <terminatoranshul@gmail.com>
2026-09-20 00:03:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ const appointmentRoutes = require('./routes/appointments');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
 // Middleware
 app.use(cors());
 app.use(express.json());
